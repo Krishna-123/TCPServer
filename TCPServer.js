98ca506a12b2e4c7f50e5f036fa49cfc223def34
krishna-123 <krishna11agarwal@gmail.com>
2017-08-24 20:37:54 +0530

    initial commit

--- TCPServer.js
+++ TCPServer.js
@@ -20,20 +20,20 @@ function handleConnection(conn) {
   console.log('new client connection from %s', remoteAddress);
 
   conn.on('data', onConnData);
-  //conn.once('close', onConnClose);
-  //conn.on('error', onConnError);
+  conn.once('close', onConnClose);
+  conn.on('error', onConnError);
 
   function onConnData(d) {
    // console.log('connection data from %s: %j', remoteAddress, d);
     console.log('data is ' + d);
    // conn.write(d);
   }
-/*
+
   function onConnClose() {
     console.log('connection from %s closed', remoteAddress);
   }
 
   function onConnError(err) {
     console.log('Connection %s error: %s', remoteAddress, err.message);
-  }*/
+  }
 }
